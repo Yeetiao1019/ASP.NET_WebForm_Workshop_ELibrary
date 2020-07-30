68e65313fa1ff0de06c5529329b4e5c898a78f78
YeeTiao1019 <n1533512@gmail.com>
2020-07-31 02:48:26 +0800

    NavBar LinkButtons connect to pages Hide "Logout"&"Hello User"
    Hide all footer management LinkButtons
    
    Add ViewBooks Page

--- WebSite/MasterPage.master.cs
+++ WebSite/MasterPage.master.cs
@@ -11,4 +11,49 @@ public partial class MasterPage : System.Web.UI.MasterPage
     {
 
     }
+
+    protected void AdminLoginLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AdminLogin.aspx");
+    }
+
+    protected void AuthorManagementLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AuthorManagement.aspx");
+    }
+
+    protected void PublisherManagementLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("PublisherManagement.aspx");
+    }
+
+    protected void BookInventoryLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("BookInventory.aspx");
+    }
+
+    protected void BookIssuingLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("BookIssuing.aspx");
+    }
+
+    protected void UserManagementLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("UserManagement.aspx");
+    }
+
+    protected void ViewBooksLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ViewBooks.aspx");
+    }
+
+    protected void UserLoginLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("UserLogin.aspx");
+    }
+
+    protected void SignUpLinkButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("SignUpPage.aspx");
+    }
 }
---
+++ WebSite/ViewBooks.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ViewBooks.aspx.cs" Inherits="ViewBooks" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="container">
+        <div class="row">
+            <div class="col mx-auto">
+                <div class="row">
+                    <div class="col">
+                        <center>
+                           <h4>Book Inventory List</h4>
+                        </center>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <asp:GridView CssClass="table table-striped table-bordered" ID="BookInventoryListGridView" runat="server"></asp:GridView>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <a href="HomePage.aspx"><< Back to Home</a><span class="clearfix"></span>
+        <br />
+    </div>
+</asp:Content>
+
